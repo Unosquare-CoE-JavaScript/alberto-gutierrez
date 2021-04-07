8e2f899c80c64034290ce8c418f91008bd2f2fd8
Alberto <albertogmx91@gmail.com>
2021-04-07 18:27:18 -0500

    Will add the hability to create products
    Creation of product service and format
    the response of product controller.

--- controller/ProductsController.js
+++ controller/ProductsController.js
@@ -1,6 +1,21 @@
+const ProductService = require("../services/products/ProductService");
+
 class ProductController {
-  create(req, res, next) {
-    res.json(req.body);
+  async create(req, res, next) {
+    const service = ProductService;
+    const response = {};
+    try {
+      const product = await service.create(req.body);
+      response.status = 200; //redundante
+      response.message = `Success`;
+      response.content = product;
+    } catch (e) {
+      response.status = 500; //redundante
+      response.message = e.message;
+      response.content = req.body;
+    }
+
+    res.status(response.status).json(response);
   }
   get() {}
   show() {}
--- services/products/ProductService.js
+++ services/products/ProductService.js
@@ -2,7 +2,9 @@ const Product = require("../../db/models/product");
 
 class ProductService {
   async create(productData) {
-    const product = new Product();
-    product.save();
+    const product = new Product({ ...productData });
+    return await product.save();
   }
 }
+
+module.exports = new ProductService();
